6417ac70b8f0667124ca2ee608baafdbc25ecf01
Ublim <ublimjo@gmail.com>
2018-04-13 10:28:39 +0300

    Update path.py
    add some docstring and fix global path error

--- telo/util/path.py
+++ telo/util/path.py
@@ -6,6 +6,7 @@ cwd = os.getcwd
 home = os.path.expanduser('~')
 
 def localP():
+    '''Get local portable path with telo database'''
     try:
         while not '.telo' in os.listdir(cwd()):
             if cwd() == '/':
@@ -18,10 +19,16 @@ def localP():
         return None
 
 def globalP():
+    '''Get global device path with telo database'''
     share = os.path.abspath(os.path.join(home, '../', 'usr/', 'share/'))
-    return os.path.join(share, 'telo')
+    result = os.path.join(share, 'telo')
+    if os.path.isdir(result):
+        return result
+    else:
+        return None
 
 def workingPath():
+    '''Get local path or global path'''
     if localP():
         return localP()
     else:
